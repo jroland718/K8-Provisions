497ac5dd65e1600c848383266a7c01dd0d109116
John <jroland718@gmail.com>
2024-11-03 22:10:14 -0500

    add firewall

--- setup-k8-master.sh
+++ setup-k8-master.sh
@@ -1,8 +1,17 @@
 sudo hostconnectl set-hostname k8s-control
 #need to setup hostfile for names of each node and master
-cat << EDF | sudo tee /etc/modules-load.d/containerd.conf
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 >overlay
 >br_netfilter
 >EOF
+
+# 
 sudo modprobe overlay
 sudo modprobe br_netfilter
+
+# nhyrfngffgf
+cat <<EOF  | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+> net.bridge.bridge-nf-call-iptables =1
+> net.ipv4.ip.forward                 =1
+> net.bridge.bridge-nf-call-ip6tables =1
+>EOF
